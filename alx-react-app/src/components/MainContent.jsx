fe55bd23db17638290ef3b80cb01c34b9688c1c7
Suhe1l <astudiobyad@gmail.com>
2024-08-09 17:00:49 +0300

    Corrected missing full stop

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent () {
     return (
         <main>
-            <p>I love to visit New York, Paris, and Tokyo</p>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
