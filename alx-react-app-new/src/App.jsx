1de149a2b92d9067617e83a60f4d987e12fd04fc
Suhe1l <astudiobyad@gmail.com>
2024-08-13 08:24:13 +0300

    Added counter component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,8 +5,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +23,7 @@ function App() {
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
+      <Counter />
       <Footer />
     </>
   )
