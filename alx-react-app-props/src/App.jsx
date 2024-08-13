61ba02f12edca71674bf2abd75cb8419a4c0d96c
Suhe1l <astudiobyad@gmail.com>
2024-08-13 16:07:43 +0300

    Added UserProfile.jsx and staged App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext'; // Import the context
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext'; // Import the context
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
