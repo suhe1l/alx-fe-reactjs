9c4e8355ddacde27a31388aa9ef91d2e6b14faea
Suhe1l <astudiobyad@gmail.com>
2024-08-13 15:49:21 +0300

    Take 1 of removing prop drilling

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,8 @@
+import UserInfo from './UserInfo';
+import React from 'react';
+
+function ProfilePage() {
+  return <UserInfo />; //No props needed
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -0,0 +1,8 @@
+import UserDetails from './UserDetails';
+import React from 'react';
+
+function UserInfo() {
+  return <UserDetails />; //No props needed
+}
+
+export default UserInfo;
