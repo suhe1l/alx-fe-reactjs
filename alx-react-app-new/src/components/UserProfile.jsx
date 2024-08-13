213b803e8fe230e4d792f77a06ae850ef1d6bb40
Suhe1l <astudiobyad@gmail.com>
2024-08-13 08:02:18 +0300

    added 'solid' and '10px'

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 const UserProfile = (props) => {
     return (
-        <div style={{ border: '1px sold gray', padding: '1em', margin: '1em' }}>
-            <h2 style={{ color: 'blueviolet' }}>{props.name}</h2>
+        <div style={{ border: '1px solid gray', padding: '1em', margin: '1em' }}>
+            <h2 style={{ color: 'blueviolet', fontSize: '10px' }}>{props.name}</h2>
             <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
         </div>
