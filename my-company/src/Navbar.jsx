8395a613d9c2af6d8ff900fc45c86b576a55851f
Suhe1l <astudiobyad@gmail.com>
2024-08-16 09:25:49 +0300

    trial 1

---
+++ my-company/src/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{
+      background: '#333',
+      padding: '10px',
+      display: 'flex',
+      justifyContent: 'space-around'
+    }}>
+      <Link to="/" style={linkStyle}>Home</Link>
+      <Link to="/about" style={linkStyle}>About</Link>
+      <Link to="/services" style={linkStyle}>Services</Link>
+      <Link to="/contact" style={linkStyle}>Contact</Link>
+    </nav>
+  );
+}
+
+const linkStyle = {
+  color: 'white',
+  textDecoration: 'none',
+  fontSize: '18px'
+};
+
+export default Navbar;
