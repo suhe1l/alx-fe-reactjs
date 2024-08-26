cb46fd2f29c154bb20c5b7098d7f855d29da2ffc
Suhe1l <astudiobyad@gmail.com>
2024-08-26 08:29:18 +0300

    2-Initialized Zustand Store and Added Styling

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import useRecipeStore from './recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
