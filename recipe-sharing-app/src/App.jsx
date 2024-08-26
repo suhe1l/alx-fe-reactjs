3ceeaa543bbc12a4a764ef8f51743ffe4b78ba96
Suhe1l <astudiobyad@gmail.com>
2024-08-26 09:02:55 +0300

    8-Added links to each recipe

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+      <Routes>
+        <Route path='/' element={
+          <>
+            <AddRecipeForm />
+            <RecipeList />
+          </>
+        } />
+        <Route path='/recipe/:id' element={<RecipeDetails />} />
+      </Routes>
     </div>
+    </Router>
+    
   );
 }
 
