d506dc0df83f6ae49cfafe07842fc6c456f7f480
Suhe1l <astudiobyad@gmail.com>
2024-08-26 08:40:43 +0300

    4-Updated Zustand Store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,6 +3,8 @@ import { create } from 'zustand'
 const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  deleteRecipe: (id) => set((state) => ({recipes: state.recipes.filter(recipe => recipe.id !== id)})),
+  updateRecipe: (id, updatedRecipe) => set((state) => ({recipes: state.recipes.map(recipe => recipe.id === id ? { ...recipe, ...updatedRecipe} : recipe )})),
   setRecipes: (recipes) => set({ recipes })
 }));
 
