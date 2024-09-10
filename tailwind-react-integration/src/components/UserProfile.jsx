bc2ec0991c7268a0fb85d77dc2df06a452594385
Suhe1l <astudiobyad@gmail.com>
2024-09-10 08:58:22 +0300

    added interactivity and transitions like hover styles

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,9 +3,9 @@
 
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-sm sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg" >
-        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"/>
-        <h1 className="text-blue-800 my-4 text-lg sm:text-lg md:text-xl">John Doe</h1>
+      <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-sm sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl" >
+        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"/>
+        <h1 className="text-blue-800 hover:text-blue-500 my-4 text-lg sm:text-lg md:text-xl">John Doe</h1>
         <p className="text-gray-600 text-sm sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
