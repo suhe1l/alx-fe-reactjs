64166d06a7c662f35be392da9aa4fc1604714a5b
Suhe1l <astudiobyad@gmail.com>
2024-09-13 18:02:05 +0300

    added navigation by using Link from react-router

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import recipesData from '../data.json'
+import { Link } from "react-router-dom";
 
 function HomePage() {
     const [ recipes, setRecipes ] = useState([]);
@@ -14,9 +15,11 @@ function HomePage() {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 {recipes.map((recipe) => (
                     <div key={recipe.id} className="bg-white rounded-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300 shadow-lg p-4">
-                        <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover rounded-lg" />
-                        <h2 className="text-xl font-semibold mt-4">{recipe.title}</h2>
-                        <p className="text-gray-600 mt-2">{recipe.summary}</p>
+                        <Link to={`/recipe/${recipe.id}`}>
+                            <img src={recipe.image} alt={recipe.title} className="w-full h-48 object-cover rounded-lg" />
+                            <h2 className="text-xl font-semibold mt-4">{recipe.title}</h2>
+                            <p className="text-gray-600 mt-2">{recipe.summary}</p>
+                        </Link>
                     </div>
                 ))}
             </div>
